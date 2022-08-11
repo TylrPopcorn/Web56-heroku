d48f2ffe3bc0153eda9f873b10f9be0f09ffb647
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-11 12:19:33 -0400

    set up the server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("test");
+/*
+    ----Used for catching errors:
+    npx eslint --init (problems, commonjs, none, no, NODE only, JSON, yes, npm)
+
+    npm install express
+    npm i -D nodemon
+*/
+
+console.log("Starting");
+
+const server = require("./api/server")
+
+const PORT = 9000;
+
+server.listen(PORT, () => {
+    console.log("Listening on port: ", PORT)
+})
